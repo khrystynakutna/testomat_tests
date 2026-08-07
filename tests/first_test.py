5b34bbb3795f3b118e2cbb3e8241578d5ff129a4
Khrystyna Kutna <khrystynakutna@gmail.com>
2026-08-07 17:31:58 +0300

    Refactoring using DRY

--- tests/first_test.py
+++ tests/first_test.py
@@ -2,7 +2,7 @@ from playwright.sync_api import Page, expect
 
 
 def test_login_with_invalid_creds(page: Page):
-    page.goto('https://testomat.io/')
+    open_home_page(page)
     expect(page).to_have_title("AI Test Management Tool | Testomat.io")
     expect(page.get_by_text("Log in", exact=True)).to_be_visible()
     page.get_by_text("Log in", exact=True).click()
@@ -13,14 +13,12 @@ def test_login_with_invalid_creds(page: Page):
     expect(page.locator("#content-desktop .common-flash-info")).to_have_text("Invalid email or password.")
 
 def test_search_project(page: Page):
-    page.goto('https://testomat.io/')
-    page.get_by_text("Log in", exact=True).click()
+    page.goto('https://app.testomat.io/users/sign_in')
 
     login_user(page, email="khrystynakutna.fa@gmail.com", password="k638Ln!r!2QucYT")
 
     target_project = "python manufacture"
-    expect(page.get_by_role("searchbox", name="Search")).to_be_visible()
-    page.locator("#content-desktop #search").fill(target_project)
+    search_for_project(page, target_project)
 
     expect(page.get_by_role("heading", name=target_project)).to_be_visible()
     expect(page.locator("ul li h3", has_text=target_project)).to_be_visible()
@@ -30,3 +28,10 @@ def login_user(page: Page, email: str, password: str):
     page.locator("#content-desktop #user_email").fill(email)
     page.locator("#content-desktop #user_password").fill(password)
     page.get_by_role("button", name="Sign In").click()
+
+def open_home_page(page: Page):
+    page.goto('https://testomat.io/')
+
+def search_for_project(page: Page, target_project: str):
+    expect(page.get_by_role("searchbox", name="Search")).to_be_visible()
+    page.locator("#content-desktop #search").fill(target_project)
