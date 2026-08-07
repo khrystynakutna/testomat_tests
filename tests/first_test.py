d74f38cefee24f2fb22220e2db76080b9dc52f04
Khrystyna Kutna <khrystynakutna@gmail.com>
2026-08-07 17:11:58 +0300

    Refactoring using KISS

--- tests/first_test.py
+++ tests/first_test.py
@@ -6,18 +6,17 @@ def test_login_with_invalid_creds(page: Page):
     expect(page).to_have_title("AI Test Management Tool | Testomat.io")
     expect(page.get_by_text("Log in", exact=True)).to_be_visible()
     page.get_by_text("Log in", exact=True).click()
-    page.locator("#content-desktop #user_email").fill("khrystynakutna.fa@gmail.com")
-    page.locator("#content-desktop #user_password").fill("k638Ln!r!2QucYе")
-    page.get_by_role("button", name="Sign In").click()
+
+    login_user(page, email="khrystynakutna.fa@gmail.com", password="kfghjjk")
+
     expect(page.locator("#content-desktop").get_by_text("Invalid Email or password.")).to_be_visible()
     expect(page.locator("#content-desktop .common-flash-info")).to_have_text("Invalid email or password.")
 
 def test_search_project(page: Page):
     page.goto('https://testomat.io/')
     page.get_by_text("Log in", exact=True).click()
-    page.locator("#content-desktop #user_email").fill("khrystynakutna.fa@gmail.com")
-    page.locator("#content-desktop #user_password").fill("k638Ln!r!2QucYT")
-    page.get_by_role("button", name="Sign In").click()
+
+    login_user(page, email="khrystynakutna.fa@gmail.com", password="k638Ln!r!2QucYT")
 
     target_project = "python manufacture"
     expect(page.get_by_role("searchbox", name="Search")).to_be_visible()
@@ -26,3 +25,8 @@ def test_search_project(page: Page):
     expect(page.get_by_role("heading", name=target_project)).to_be_visible()
     expect(page.locator("ul li h3", has_text=target_project)).to_be_visible()
     expect(page.locator(selector="ul li h3").filter(has_text=target_project)).to_be_visible()
+
+def login_user(page: Page, email: str, password: str):
+    page.locator("#content-desktop #user_email").fill(email)
+    page.locator("#content-desktop #user_password").fill(password)
+    page.get_by_role("button", name="Sign In").click()
