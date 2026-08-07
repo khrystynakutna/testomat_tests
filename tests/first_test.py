0c2252645445813ca3e9e8e9c1fdbbd23c1422d9
Khrystyna Kutna <khrystynakutna@gmail.com>
2026-08-08 02:28:58 +0300

    Refactoring code

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,4 +1,8 @@
+import os
+from dotenv import load_dotenv
 from playwright.sync_api import Page, expect
+
+load_dotenv()
 
 
 def test_login_with_invalid_creds(page: Page):
@@ -7,15 +11,15 @@ def test_login_with_invalid_creds(page: Page):
     expect(page.get_by_text("Log in", exact=True)).to_be_visible()
     page.get_by_text("Log in", exact=True).click()
 
-    login_user(page, email="khrystynakutna.fa@gmail.com", password="kfghjjk")
+    login_user(page, email=os.getenv("EMAIL"), password="kfghjjk")
 
     expect(page.locator("#content-desktop").get_by_text("Invalid Email or password.")).to_be_visible()
     expect(page.locator("#content-desktop .common-flash-info")).to_have_text("Invalid email or password.")
 
 def test_search_project(page: Page):
-    page.goto('https://app.testomat.io/users/sign_in')
+    page.goto(f"{os.getenv("BASE_APP_URL")}/users/sign_in")
 
-    login_user(page, email="khrystynakutna.fa@gmail.com", password="k638Ln!r!2QucYT")
+    login_user(page, email=os.getenv("EMAIL"), password=os.getenv("PASSWORD"))
 
     target_project = "python manufacture"
     search_for_project(page, target_project)
@@ -25,22 +29,25 @@ def test_search_project(page: Page):
     expect(page.locator(selector="ul li h3").filter(has_text=target_project)).to_be_visible()
 
 def test_open_free_project(page: Page):
-    page.goto('https://app.testomat.io/users/sign_in')
-    login_user(page, email="khrystynakutna.fa@gmail.com", password="k638Ln!r!2QucYT")
+    #arrange
+    page.goto(f"{os.getenv("BASE_APP_URL")}/users/sign_in")
+    login_user(page, email=os.getenv("EMAIL"), password=os.getenv("PASSWORD"))
+    #act
     page.locator("#company_id").click()
     page.locator("#company_id").select_option("Free Projects")
 
+    #assert
     target_project = "python manufacture"
     search_for_project(page, target_project)
     expect(page.get_by_role("heading", name=target_project)).to_be_hidden()
 
-def login_user(page: Page, email: str, password: str):
+def login_user(page: Page, email, password):
     page.locator("#content-desktop #user_email").fill(email)
     page.locator("#content-desktop #user_password").fill(password)
     page.get_by_role("button", name="Sign In").click()
 
 def open_home_page(page: Page):
-    page.goto('https://testomat.io/')
+    page.goto(os.getenv("BASE_URL"))
 
 def search_for_project(page: Page, target_project: str):
     expect(page.get_by_role("searchbox", name="Search")).to_be_visible()
