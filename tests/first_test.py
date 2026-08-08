bd90542415fd7ed2149f1e94703f57407275b53f
Khrystyna Kutna <khrystynakutna@gmail.com>
2026-08-08 03:41:17 +0300

    Refactoring with dataclasses,faker, fixtures

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,47 +1,53 @@
 import os
 
+import pytest
+from faker import Faker
 from playwright.sync_api import Page, expect
 
-from tests.conftest import configs
+from tests.conftest import configs, Config
 
 
-def test_login_with_invalid_creds(page: Page,configs: dict):
+TARGET_PROJECT= "python manufacture"
+
+@pytest.fixture(scope="function")
+def login(page: Page, configs: Config):
+    page.goto(configs.login_url)
+    login_user(page, email=(configs.email), password=(configs.password))
+
+
+def test_login_with_invalid_creds(page: Page,configs: Config):
     open_home_page(page)
 
     expect(page).to_have_title("AI Test Management Tool | Testomat.io")
     expect(page.get_by_text("Log in", exact=True)).to_be_visible()
     page.get_by_text("Log in", exact=True).click()
 
-    login_user(page, email=(configs["email"]), password="kfghjjk")
+    inv_paswd=Faker().password(length=10)
+    print(inv_paswd)
+
+    login_user(page, email=(configs.email), password= inv_paswd)
 
     expect(page.locator("#content-desktop").get_by_text("Invalid Email or password.")).to_be_visible()
     expect(page.locator("#content-desktop .common-flash-info")).to_have_text("Invalid email or password.")
 
-def test_search_project(page: Page, configs: dict):
-    page.goto(configs["login_url"])
+def test_search_project(page: Page,login):
 
-    login_user(page, email=(configs["email"]), password=(configs["password"]))
 
-    target_project = "python manufacture"
-    search_for_project(page, target_project)
+    search_for_project(page, TARGET_PROJECT)
 
-    expect(page.get_by_role("heading", name=target_project)).to_be_visible()
-    expect(page.locator("ul li h3", has_text=target_project)).to_be_visible()
-    expect(page.locator(selector="ul li h3").filter(has_text=target_project)).to_be_visible()
+    expect(page.get_by_role("heading", name=TARGET_PROJECT)).to_be_visible()
+    expect(page.locator("ul li h3", has_text=TARGET_PROJECT)).to_be_visible()
+    expect(page.locator(selector="ul li h3").filter(has_text=TARGET_PROJECT)).to_be_visible()
 
-def test_open_free_project(page: Page,configs: dict):
-    #arrange
-    page.goto(configs["login_url"])
-    login_user(page, email=(configs["email"]), password=(configs["password"]))
+def test_open_free_project(page: Page,login):
 
     #act
     page.locator("#company_id").click()
     page.locator("#company_id").select_option("Free Projects")
 
     #assert
-    target_project = "python manufacture"
-    search_for_project(page, target_project)
-    expect(page.get_by_role("heading", name=target_project)).to_be_hidden()
+    search_for_project(page, TARGET_PROJECT)
+    expect(page.get_by_role("heading", name=TARGET_PROJECT)).to_be_hidden()
 
 def login_user(page: Page, email, password):
     page.locator("#content-desktop #user_email").fill(email)
