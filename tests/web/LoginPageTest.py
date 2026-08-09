157d68c5b787d107cd43b71c5959d4c5dd28d92f
Khrystyna Kutna <khrystynakutna@gmail.com>
2026-08-09 22:28:28 +0300

    Added tests using page object

---
+++ tests/web/LoginPageTest.py
@@ -0,0 +1,21 @@
+import faker
+from faker import Faker
+from playwright.sync_api import Page
+
+from src.web.pages.HomePage import HomePage
+from src.web.pages.LoginPage import LoginPage
+from tests.conftest import configs, Config
+
+
+def test_login_invalid(page: Page, configs: Config):
+    home_page = HomePage(page)
+    home_page.open()
+    home_page.is_loaded()
+    home_page.click_login()
+
+    login_page = LoginPage(page)
+    login_page.is_loaded()
+    login_page.login(email=configs.email, password= Faker().password(length=10))
+    login_page.invalid_message_visible()
+
+
